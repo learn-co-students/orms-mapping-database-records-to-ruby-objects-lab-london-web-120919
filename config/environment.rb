5eba9f100ca00e9d73b6a5ca0f19d49761b818df
rmiri <re_miriuk@hotmail.com>
2019-12-18 15:52:41 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,10 @@
+require "pry"
+
 require 'sqlite3'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+# binding.pry
+
+# puts "Just hang the students in the ceiling fan if they misbhave"
